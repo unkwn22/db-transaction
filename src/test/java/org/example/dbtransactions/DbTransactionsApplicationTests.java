27ce94772b23abf84b1ff964b8e93c337af7867d
unkwn <lsjc12911@naver.com>
2024-04-29 13:29:42 +0900

    first commit

---
+++ src/test/java/org/example/dbtransactions/DbTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dbtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
